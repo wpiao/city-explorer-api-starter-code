b898304c6086cb54a9f376fb8358e02583eccbf7
Wenhao Piao <wenhaopiao520@gmail.com>
2021-04-21 19:55:04 -0700

    Add required dependencies

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 'use strict';
 
-require('dotenv');
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
 const weather = require('./modules/weather.js');
 const app = express();
 
+app.use(cors());
 app.get('/weather', weatherHandler);
 
 function weatherHandler(request, response) {
   const { lat, lon } = request.query;
   weather(lat, lon)
-  .then(summaries => response.send(summaries))
-  .catch((error) => {
-    console.error(error);
-    response.status(200).send('Sorry. Something went wrong!')
-  });
-}  
+    .then(summaries => response.send(summaries))
+    .catch((error) => {
+      console.error(error);
+      response.status(200).send('Sorry. Something went wrong!')
+    });
+}
 
 app.listen(process.env.PORT, () => console.log(`Server up on ${process.env.PORT}`));
